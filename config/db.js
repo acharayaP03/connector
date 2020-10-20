cdcfc1bfb30850580e779bde73689b088c871757
Prabhakar Acharya <acharyaP03@gmail.com>
2020-10-20 20:41:16 +1100

    Connect Db

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI')
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(db,{
+            useUnifiedTopology:true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log("Database is connected...")
+    }catch(err){
+        console.log(err.message);
+        //if error exit the process
+        process.exit(1)
+    }
+}
+module.exports = connectDB;
