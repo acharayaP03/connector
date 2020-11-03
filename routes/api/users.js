e364c1224b2ffd66c32f4e0e7aa22ff55b23bb5f
Prabhakar Acharya <acharyaP03@gmail.com>
2020-11-03 19:52:53 +1100

    password hasing and jwt implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,14 @@
 const express = require('express');
+
 const router = express.Router();
+
+const gravatar = require('gravatar');
+
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+const config = require('config');
 
 // using express validator to validate user's reques.
 /**
@@ -10,6 +19,7 @@ const router = express.Router();
 
 const { check, validationResult } = require('express-validator');
 
+const User = require('../../models/User');
 /**
  * @route   POST api/users
  * @description Test Route
@@ -21,15 +31,70 @@ router.post(
   [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please enter a valid email.').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters.'),
+    check('password', 'Please enter a password with 6 or more characters.').isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('Users Route.');
+    const { name, email, password } = req.body;
+
+    try {
+      // check if user already exist.
+      let user = await User.findOne({ email });
+
+      if (user) {
+        // if the return statement not present then this will throw headers already sent error.
+
+        return res.status(400).json({ error: [{ msg: 'User already exists' }] });
+      }
+      // Get users gravatar.
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt Password.
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // return jwt.
+
+      const paylaod = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        paylaod,
+        config.get('jwtsecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+
+      res.status(500).send('Something went wrong, please try again later.');
+    }
   }
 );
 
