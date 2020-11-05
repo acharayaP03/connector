0ef54ab63771ddc1abeefe1a3aad526096e95e56
Prabhakar Acharya <acharyaP03@gmail.com>
2020-11-05 13:36:02 +1100

    api/profile route to create and update user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,120 @@
 const express = require('express');
+
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
-/**
- * @route   GET api/profile
- * @description Test Route
- * @access  Public
-*/
+const auth = require('../../middleware/auth');
 
-router.get('/', (req, res) =>{
-    res.send("Profile Route.")
+const Profile = require('../../models/Profile');
+
+/**
+ * @route   GET api/profile/me
+ * @description get profile for the logged in user
+ * @access  Private
+ */
+
+router.get('/me', auth, async (req, res) => {
+  const { id } = req.user;
+  try {
+    const profile = await Profile.findOne({ user: id }).populate('user', 'name', 'avatar');
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'User profile doesnt exits.' });
+    }
+  } catch (err) {
+    console.log(err.message);
+
+    res.status(500).send('Server Error');
+  }
 });
+
+/**
+ * @route   GET api/profile/
+ * @description Create or Update user profile
+ * @access  Private
+ */
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    // build porfile object
+    const profileFields = {};
+    // since token that was sent will have user id along with it, we can extract most of the details from it to send it to mongo db.
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+
+    // since we need to turn skills comes as comma separated, we need to turn that into arrar.
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+    // console.log(profileFields.skills); // [ 'html', 'php', 'ruby', 'react' ]
+
+    // Build Social object
+    profileFields.social = {};
+
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        // id profile exist then update.
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      // if porfile doesnt exist then create it.
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      return res.json(profile);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+
+    res.send('Hello');
+  }
+);
 
 module.exports = router;
