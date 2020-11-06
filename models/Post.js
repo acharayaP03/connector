a8d6195907f1cb99495e316beab8592adda952ad
Prabhakar Acharya <acharyaP03@gmail.com>
2020-11-06 19:28:00 +1100

    Post model

---
+++ models/Post.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require('mongoose');
+
+/**
+ * @params Post model will allow user to create post
+ * @Create likes and comments
+ * @Constraint only one likes and one comments per user.
+ * @post post will be visible even if the user is deleted.
+ */
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = model('profile', PostSchema);
+
+module.exports = Post;
